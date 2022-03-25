55873c89afba7600676690d163a897ed40b75e8b
Ray East <raydeast@gmail.com>
2022-03-25 18:39:30 +0530

    :ukraine: #StandWithUkraine Improve Job Console Output

--- jobs/handle-job.ts
+++ jobs/handle-job.ts
@@ -8,6 +8,7 @@ const token = process.env.JOBS_API_TOKEN!;
 
 export default function handleJob(name: string): void {
   axios.get(`${host}/api/jobs/${name}?token=${token}`)
+    .then(() => console.log(`The job \`${name}\` has been successfully initiated. Please see the server's logs for additional output upon completion.`))
     .catch((error) => {
       const hasErrorTextInResponse = axios.isAxiosError(error)
         && error.response
@@ -16,6 +17,8 @@ export default function handleJob(name: string): void {
 
       if (hasErrorTextInResponse) {
         console.log((error as AxiosError).response!.data!.message!);
+
+        return;
       }
 
       const hasOtherErrorText = error instanceof Error && error.message;
