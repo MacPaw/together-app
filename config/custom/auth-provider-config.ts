9695f4541d1b3816edc594231f2bfac796988799
Dima Barbashov <tgskiv@gmail.com>
2022-03-27 18:00:24 +0300

    Enable Slack OAuth

--- config/custom/auth-provider-config.ts
+++ config/custom/auth-provider-config.ts
@@ -1,3 +1,13 @@
 import type { Nullable, AuthProviderConfig } from '../../types';
+import SlackProvider from 'next-auth/providers/slack';
 
-export const authProviderConfig: Nullable<AuthProviderConfig> = null;
+const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID!;
+const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET!;
+
+export const authProviderConfig: Nullable<AuthProviderConfig> = {
+  provider: SlackProvider({
+    clientId: SLACK_CLIENT_ID,
+    clientSecret: SLACK_CLIENT_SECRET,
+  }),
+  type: 'slack',
+};
